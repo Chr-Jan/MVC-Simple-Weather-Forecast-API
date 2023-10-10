c8e17627b411bd472c95292db92be205eb0b6821
Christoffer Jansson <christofferjansson1@gmail.com>
2023-10-10 10:22:49 +0200

    update

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WeatherMap.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
